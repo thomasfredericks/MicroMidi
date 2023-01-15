40a173158481c812eccf37a531190add05c5839f
Fredericks <thomas@fredericks.info>
2023-01-14 22:06:11 -0500

    Another Fix for Running Status

--- src/MicroMidi.h
+++ src/MicroMidi.h
@@ -6,163 +6,137 @@ A recommended approach for a receiving device is to maintain its "running status
     Buffer is cleared (ie, set to 0) at power up.
     Buffer stores the status when a Voice Category Status (ie, 0x80 to 0xEF) is received.
     Buffer is cleared when a System Common Category Status (ie, 0xF0 to 0xF7) is received.
-    Nothing is done to the buffer when a RealTime Category message is received (ie, Status of 0xF8 to 0xFF). 
-        Because a RealTime message may be received at any time, including interspersed with another message, it should be handled transparently. For example, if a 0xF8 byte was received inbetween any 2 bytes of the above examples, the 0xF8 should be processed immediately, and then the device should resume processing the example streams exactly as it would have otherwise. Because RealTime messages only consist of a Status, running status obviously can't be implemented on RealTime messages. 
+    Nothing is done to the buffer when a RealTime Category message is received (ie, Status of 0xF8 to 0xFF).
+        Because a RealTime message may be received at any time, including interspersed with another message, it should be handled transparently. For example, if a 0xF8 byte was received inbetween any 2 bytes of the above examples, the 0xF8 should be processed immediately, and then the device should resume processing the example streams exactly as it would have otherwise. Because RealTime messages only consist of a Status, running status obviously can't be implemented on RealTime messages.
     Any data bytes are ignored when the buffer is 0.
 
 */
 
 class MicroMidi {
 
-    void (*noteOnCallback)(byte channel, byte note, byte velocity);
-    void (*noteOffCallback)(byte channel, byte note);
-    void (*controlChangeCallback) (byte channel, byte controller, byte value);
+  void (*noteOnCallback)(byte channel, byte note, byte velocity);
+  void (*noteOffCallback)(byte channel, byte note);
+  void (*controlChangeCallback) (byte channel, byte controller, byte value);
 
-  private:
-    byte  midiType;
-    byte midiChannel;
+private:
+  byte midiChannel;
+  byte runningStatusIn;
 
-    uint8_t midiMessageLength = 0;
-    byte midiMessage[3];
-    Stream * stream;
+  uint8_t midiMessageLength = 0;
+  byte midiMessage[3];
+  Stream * stream;
 
-    const static byte TINY_MIDI_NOTE_OFF = 0x80; 
-    const static byte TINY_MIDI_NOTE_ON = 0x90; 
-    const static byte TINY_MIDI_CTL = 0xB0;
-
-
-  public:
-   
-    const static unsigned long MIDI_SERIAL_BAUD = 31250;
-    const static int MIDI_SERIAL_CONFIG = SERIAL_8N1;
+  const static byte TINY_MIDI_NOTE_OFF = 0x80;
+  const static byte TINY_MIDI_NOTE_ON = 0x90;
+  const static byte TINY_MIDI_CTL = 0xB0;
 
 
-    /*
-      #define MM_NOTE_OFF 0x08
-      #define MM_NOTE_ON 0x09
-      #define MM_CTL 0x0B
-      #define MM_BAUD 31250
-    */
-    MicroMidi( Stream * stream) {
-      this->stream = stream;
-    }
+public:
 
-    void setControlChangeCallback(void (*fptr)(byte channel, byte controller, byte value)) {
-      controlChangeCallback = fptr;
-    }
+  const static unsigned long MIDI_SERIAL_BAUD = 31250;
+  const static int MIDI_SERIAL_CONFIG = SERIAL_8N1;
 
-    void setMidiNoteOnCallback(void (*fptr)(byte channel, byte note, byte velocity)) {
-      noteOnCallback = fptr;
-    }
 
-    void setMidiNoteOffCallback(void (*fptr)(byte channel, byte note)) {
-      noteOffCallback = fptr;
-    }
-/*
-    void dd(int indata) {
-        Serial.print(indata);
-        Serial.print("->");
-        Serial.print(midiType);
-        Serial.print(" ");
-        Serial.print(midiMessageLength);
-        Serial.print(" ");
-        Serial.print(midiMessage[0]);
-        Serial.print(" ");
-        Serial.print(midiMessage[1]);
-        Serial.println();
-    }
-*/
-    void receiveMessages() {
-      while (stream->available() ) {
-        int data = stream->read();
+  /*
+    #define MM_NOTE_OFF 0x08
+    #define MM_NOTE_ON 0x09
+    #define MM_CTL 0x0B
+    #define MM_BAUD 31250
+  */
+  MicroMidi( Stream * stream) {
+    this->stream = stream;
+  }
 
-        int indata= data;
-        //Serial.print("RAW ");
-        //Serial.println(data);
+  void setControlChangeCallback(void (*fptr)(byte channel, byte controller, byte value)) {
+    controlChangeCallback = fptr;
+  }
 
-        if (data < 0x80) { // 0x80 == 128 == B10000000
+  void setMidiNoteOnCallback(void (*fptr)(byte channel, byte note, byte velocity)) {
+    noteOnCallback = fptr;
+  }
 
-          if ( midiType >= 0x80 && midiType < 0xF0  ) {
+  void setMidiNoteOffCallback(void (*fptr)(byte channel, byte note)) {
+    noteOffCallback = fptr;
+  }
+  /*
+      void dd(int indata) {
+          Serial.print(indata);
+          Serial.print("->");
+          Serial.print(midiType);
+          Serial.print(" ");
+          Serial.print(midiMessageLength);
+          Serial.print(" ");
+          Serial.print(midiMessage[0]);
+          Serial.print(" ");
+          Serial.print(midiMessage[1]);
+          Serial.println();
+      }
+  */
+  void receiveMessages() {
+    while (stream->available() ) {
+      int data = stream->read();
 
+      int indata = data;
+      //Serial.print("RAW ");
+      //Serial.println(data);
+
+      if (data < 0x80) { // 0x80 == 128 == B10000000
+        if ( runningStatusIn && runningStatusIn < 0xF0 ) {
+         
             midiMessage[midiMessageLength] = data;
             midiMessageLength++;
 
-            /*
-            Serial.print("midiBuffer ");
-            Serial.print(midiMessage[0]);
-            Serial.print(" ");
-
-            if (midiMessageLength> 1) {
-              Serial.println(midiMessage[1]);
-            } else {
-              Serial.println();
-            }
-
- */
-
-            
-
             if ( midiMessageLength == 2 ) {
-             // dd(indata);
-              
-              if ( midiType == TINY_MIDI_NOTE_ON    ) {
+              // dd(indata);
+
+              if ( runningStatusIn == TINY_MIDI_NOTE_ON    ) {
                 if ( midiMessage[1] > 0  ) {
                   //Serial.println("NOTE ON");
                   if ( noteOnCallback) noteOnCallback(midiChannel, midiMessage[0], midiMessage[1]);
                 } else {
-                 // Serial.println("NOTE OFF");
+                  // Serial.println("NOTE OFF");
                   if ( noteOffCallback ) noteOffCallback(midiChannel, midiMessage[0] );
                 }
-                
-              } else if ( midiType == TINY_MIDI_NOTE_OFF && noteOffCallback   ) {
-               //Serial.println("NOTE ON");
+
+              } else if ( runningStatusIn == TINY_MIDI_NOTE_OFF && noteOffCallback   ) {
+                //Serial.println("NOTE ON");
                 noteOffCallback(midiChannel, midiMessage[0] );
 
-              } else if ( midiType == TINY_MIDI_CTL && controlChangeCallback  ) {
-               //Serial.println("CTRL");
+              } else if ( runningStatusIn == TINY_MIDI_CTL && controlChangeCallback  ) {
+                //Serial.println("CTRL");
                 controlChangeCallback(midiChannel, midiMessage[0] , midiMessage[1]);
 
               }
               // KEEP MIDI TYPE (AND CHANNEL) FOR RUNNING STATUS
               //midiType = 0;
-             midiMessageLength = 0;
-            }  
-          } else {
-            midiMessageLength = 0;
-            //Serial.println("NSL");
-          }
-            /*
-            else {
-              dd(indata);
+              midiMessageLength = 0;
             }
-            */
-            //midiMessageLength = 0;
-
         } else {
-
-          // GET RID OF CHANNEL DATA
-          int incommingDataType = data & B11110000;
-          
-          //if ( data == TINY_MIDI_NOTE_OFF || data == TINY_MIDI_NOTE_ON || data == TINY_MIDI_CTL ) {
-          /*if (data >= 8 && data < 15  )
-            midiType = data;
-
-          } else {
-            midiType = 0;  
-          }*/
-          // VOICE MESSAGES
-          if ( incommingDataType >= 0x80 && incommingDataType < 0xF0 ) {
-            midiType = data;
-            // GET CHANNEL DATA AND OFFSET IT BY 1
-            midiChannel = (data & B00001111)+1;
-          }            
           midiMessageLength = 0;
         }
-        
-        //dd(indata);
+
+      } else {
+
+        // GET RID OF CHANNEL DATA
+        int incommingDataType = data & B11110000;
+
+
+        if ( incommingDataType  < 0xF0 ) {
+          // Voice Categroy Status
+          runningStatusIn = data;
+          // GET CHANNEL DATA AND OFFSET IT BY 1
+          midiChannel = (data & B00001111) + 1;
+        } else if (  incommingDataType < 0xF8 ) {
+          // System Common Category Status
+          runningStatusIn = 0;
+        }
+        midiMessageLength = 0;
       }
+
+      //dd(indata);
     }
-    };
+  }
+};
 
 template <int L>
 class MicroMidiLastNoteHeldTable {
@@ -193,13 +167,13 @@ public:
       count++;
     } else {
       shiftNotes(0);
-      data[L-1] = note;
+      data[L - 1] = note;
     }
-  
+
   }
 
-  void clear(){
-    count =0;
+  void clear() {
+    count = 0;
   }
 
   int getCount() {
@@ -207,7 +181,7 @@ public:
   }
 
   byte getActiveNote() {
-    if ( count ) return data[count-1];
+    if ( count ) return data[count - 1];
     else return 0;
   }
 
